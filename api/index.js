66b53cb827bdcd4cc41cf0c34f1f44b8d9dcd6c7
Vladislav <Vladislav_lebedkin@mail.ru>
2023-03-02 21:05:35 +0300

    [01] сделал роуты и кнекты бекенд

--- api/index.js
+++ api/index.js
@@ -8,14 +8,14 @@ import likeRoutes from "./routes/likes.js";
 const app = express();
 
 //middlewares
-app.use(express.json())
+app.use(express.json());
 
-app.use("/api/auth", authRoutes)
-app.use("/api/users", userRoutes)
-app.use("/api/posts", postRoutes)
-app.use("/api/comments", commentRoutes)
-app.use("/api/likes", likeRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
 
 app.listen(8800, ()=>{
   console.log("API working!!!");
-})
+});
